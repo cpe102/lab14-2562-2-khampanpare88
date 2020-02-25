316e97007d0d5bd71d694d6e58ea85949f6df9d0
lab302-7 <lab302-7@labeng.local>
2020-02-25 17:31:49 +0700

    lab14_1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,11 +1,60 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 
 int main(){
 	//Write your code here.
-	
-	
-	
+	int a = 5;
+	char b ='A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
+	cout << "a ="<< a <<"\n";
+	cout << "b ="<< b <<"\n";
+	cout << "c ="<< c <<"\n";
+	cout << "x ="<< x <<"\n";
+	cout << "y ="<< (int*)y <<"\n";
+	cout << "z ="<< z <<"\n";
+	cout<< "\n";
+	cout << "&a ="<< &a <<"\n";
+	cout << "&b ="<< (int*)&b <<"\n";
+	cout << "&c ="<< (int*)&c <<"\n";
+	cout << "&x ="<< &x <<"\n";
+	cout << "&y ="<< &y <<"\n";
+	cout << "&z ="<< &z <<"\n";
+	cout<< "\n";
+	c='F';
+	cout << "a ="<< a <<"\n";
+	cout << "b ="<< b <<"\n";
+	cout << "c ="<< c <<"\n";
+	cout << "x ="<< x <<"\n";
+	cout << "y ="<< (int*)y <<"\n";
+	cout << "z ="<< z <<"\n";
+	cout<< "\n";
+	*y='W';
+	cout << "a ="<< a <<"\n";
+	cout << "b ="<< b <<"\n";
+	cout << "c ="<< c <<"\n";
+	cout << "x ="<< x <<"\n";
+	cout << "y ="<< (int*)y <<"\n";
+	cout << "z ="<< z <<"\n";
+	cout<< "\n";
+	*x=6;
+	cout << "a ="<< a <<"\n";
+	cout << "b ="<< b <<"\n";
+	cout << "c ="<< c <<"\n";
+	cout << "x ="<< x <<"\n";
+	cout << "y ="<< (int*)y <<"\n";
+	cout << "z ="<< z <<"\n";
+	cout<< "\n";
+	**z=7;
+	cout << "a ="<< a <<"\n";
+	cout << "b ="<< b <<"\n";
+	cout << "c ="<< c <<"\n";
+	cout << "x ="<< x <<"\n";
+	cout << "y ="<< (int*)y <<"\n";
+	cout << "z ="<< z <<"\n";
+
 	return 0;
 }
