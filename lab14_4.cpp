878e0440a10326696d2f0340744b0e865620cc88
lab302-7 <lab302-7@labeng.local>
2020-02-25 17:33:47 +0700

    lab14_4

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -19,3 +19,25 @@ int main(){
 }
 
 //Write definition of shuffle() here 
+void shuffle(int &a,int &b, int &c, int &d){
+	int k=a,m=b,n=c,o=d;
+	int x[4]={k,m,n,o};
+	int y[]={0,1,2,3};
+	y[0] = rand()%4;
+	while(1){
+		y[1] = rand()%4;
+		if(y[1] != y[0]) break;
+	}
+	while(1){
+		y[2] = rand()%4;
+		if((y[2] != y[0]) && (y[2] != y[1])) break;
+	}
+	while(1){
+		y[3] = rand()%4;
+		if((y[3]!=y[2])&&(y[3] != y[0]) && (y[3] != y[1])) break;
+	}
+	a=x[y[0]];
+	b=x[y[1]];
+	c=x[y[2]];
+	d=x[y[3]];
+}
